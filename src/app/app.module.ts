7ebf62567028c1729267dd17cefbf761a0303282
David Toledo <dtoledoz0207@gmail.com>
2020-09-27 12:51:23 -0500

    create-authModule-to-separate-login-register-from-appModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Modules
 import { AppRoutingModule } from './app-routing.module';
 import { PagesModule } from './pages/pages.module';
+import { AuthModule } from './auth/auth.module';
 
 import { AppComponent } from './app.component';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
 import { Error404Component } from './pages/error404/error404.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    RegisterComponent,
     Error404Component
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    PagesModule
+    PagesModule,
+    AuthModule
   ],
   bootstrap: [AppComponent]
 })

a211e8d2511358f9efad92ca0f5c0f70cc547a42
David Toledo <dtoledoz0207@gmail.com>
2020-09-27 17:56:23 -0500

    create-pagesRouting-to-order-all-Components-in-pages-folder

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,36 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
+import { PagesRoutingModule } from './pages/pages.routing';
+
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
-
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { Error404Component } from './error404/error404.component';
-import { Graph1Component } from './pages/graph1/graph1.component';
-import { PagesComponent } from './pages/pages.component';
-import { ProgressComponent } from './pages/progress/progress.component';
 
 
 const routes:Routes = [
-  {
-    path: '',
-    component: PagesComponent,
-    children: [
-      {path: 'dashboard', component: DashboardComponent},
-      {path: 'progress', component: ProgressComponent},
-      {path: 'graph1', component: Graph1Component},
-      {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
-    ]
-  },
-
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
-
   {path: '**', component: Error404Component},
 ];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    PagesRoutingModule
   ],
   exports: [RouterModule]
 })

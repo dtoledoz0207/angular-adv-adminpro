fd51486edd582ddca853d505d9608e3ab1b8919b
David Toledo <dtoledoz0207@gmail.com>
2020-10-15 17:29:54 -0500

    add-unsubscribe-method-in-breadcrumbsComponent

--- src/app/shared/breadcrumbs/breadcrumbs.component.ts
+++ src/app/shared/breadcrumbs/breadcrumbs.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { ActivationEnd, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { filter, map } from 'rxjs/operators';
 
 @Component({
@@ -8,23 +9,28 @@ import { filter, map } from 'rxjs/operators';
   styles: [
   ]
 })
-export class BreadcrumbsComponent {
+export class BreadcrumbsComponent implements OnDestroy {
 
   title:string;
+  titleSubs$:Subscription;
 
   constructor(private router: Router) {
-    this.getRouteData();
-  }
-
-  getRouteData() {
-    this.router.events.pipe(
-      filter(event => event instanceof ActivationEnd),
-      filter((event: ActivationEnd) => event.snapshot.firstChild === null),
-      map((event:ActivationEnd) => event.snapshot.data)
-    ).subscribe(data => {
+    this.titleSubs$ = this.getRouteData().subscribe(data => {
       this.title = data.title;
       document.title = `AdminPro - ${data.title}`;
     });
+  }
+
+  ngOnDestroy() {
+    this.titleSubs$.unsubscribe();
+  }
+
+  getRouteData() {
+    return this.router.events.pipe(
+      filter(event => event instanceof ActivationEnd),
+      filter((event: ActivationEnd) => event.snapshot.firstChild === null),
+      map((event:ActivationEnd) => event.snapshot.data)
+    );
   }
 
 

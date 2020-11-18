d028fd3979742d54b6d9605c781e31996062bffa
David Toledo <dtoledoz0207@gmail.com>
2020-11-18 16:57:43 -0600

    add-reactiveForm-to-use-registerForm-with-formBuilder

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent {
 
-  constructor() { }
+  public registerForm = this.formBuilder.group({
+    name: ['David', Validators.required],
+    email: ['test100@gmail.com', Validators.required],
+    password: ['123456', Validators.required],
+    password2: ['123456', Validators.required],
+    terms: [false, Validators.required]
+  });
 
-  ngOnInit(): void {
+  constructor(private formBuilder: FormBuilder) { }
+
+  createUser() {
+    console.log(this.registerForm.value);
   }
 
 }

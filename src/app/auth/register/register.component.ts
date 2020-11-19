1cabc5825bf8d173e3fec903c4bedc5e7417c1a6
David Toledo <dtoledoz0207@gmail.com>
2020-11-18 18:33:19 -0600

    add-functionality-to-validate-form-in-registerComponent

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -8,18 +8,39 @@ import { FormBuilder, Validators } from '@angular/forms';
 })
 export class RegisterComponent {
 
+  public formSubmitted:boolean = false;
+
   public registerForm = this.formBuilder.group({
-    name: ['David', Validators.required],
-    email: ['test100@gmail.com', Validators.required],
-    password: ['123456', Validators.required],
-    password2: ['123456', Validators.required],
-    terms: [false, Validators.required]
+    name: ['David Toledo', Validators.required],
+    email: ['test100@gmail.com', [Validators.required, Validators.email]],
+    password: ['', Validators.required],
+    password2: ['', Validators.required],
+    terms: [null, Validators.required]
   });
 
   constructor(private formBuilder: FormBuilder) { }
 
   createUser() {
+    this.formSubmitted = true;
     console.log(this.registerForm.value);
+
+    if (this.registerForm.valid) {
+      console.log('posting form');
+    } else {
+      console.log('Invalid form');
+    }
+  }
+
+  invalidField(field:string): boolean {
+    if (this.registerForm.get(field).invalid && this.formSubmitted) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  acceptTerms() {
+    return !this.registerForm.get('terms').value && this.formSubmitted;
   }
 
 }

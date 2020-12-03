f75a4e0a07e573e0832c2e03e5d8b447e44fcc21
David Toledo <dtoledoz0207@gmail.com>
2020-12-02 22:00:16 -0600

    get-users-by-pagination-in-user-Component

--- src/app/pages/maintenance/users/users.component.ts
+++ src/app/pages/maintenance/users/users.component.ts
@@ -12,14 +12,31 @@ export class UsersComponent implements OnInit {
 
   public totalUsers:number = 0;
   public users:User[] = [];
+  public from:number = 0;
 
   constructor(private userService: UserService) { }
 
   ngOnInit(): void {
-    this.userService.loadUsers(0).subscribe(({total, users}) => {
+    this.loadUsers();
+  }
+
+  loadUsers() {
+    this.userService.loadUsers(this.from).subscribe(({total, users}) => {
       this.totalUsers = total;
       this.users = users;
     });
+  }
+
+  changePage(value:number) {
+    this.from += value;
+
+    if (this.from < 0) {
+      this.from = 0;
+    } else if (this.from >= this.totalUsers) {
+      this.from -= value;
+    }
+
+    this.loadUsers();
   }
 
 }

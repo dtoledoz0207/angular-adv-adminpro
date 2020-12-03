3af2849719a21137947050caf13e8316c1f3fbce
David Toledo <dtoledoz0207@gmail.com>
2020-12-03 17:03:03 -0600

    create-loading-variable-to-show-loadingAlert-in-users-component

--- src/app/pages/maintenance/users/users.component.ts
+++ src/app/pages/maintenance/users/users.component.ts
@@ -13,6 +13,7 @@ export class UsersComponent implements OnInit {
   public totalUsers:number = 0;
   public users:User[] = [];
   public from:number = 0;
+  public loading:boolean = true;
 
   constructor(private userService: UserService) { }
 
@@ -21,9 +22,13 @@ export class UsersComponent implements OnInit {
   }
 
   loadUsers() {
+    this.loading = true;
+
     this.userService.loadUsers(this.from).subscribe(({total, users}) => {
       this.totalUsers = total;
       this.users = users;
+
+      this.loading = false;
     });
   }
 

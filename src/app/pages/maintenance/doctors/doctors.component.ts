142289e3137f8fe7d193c68198ef2d2efb4964bd
David Toledo <dtoledoz0207@gmail.com>
2020-12-10 17:17:46 -0600

    create-CRUD-for-doctors

--- src/app/pages/maintenance/doctors/doctors.component.ts
+++ src/app/pages/maintenance/doctors/doctors.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalImageService } from '../../../services/modal-image.service';
+import { Doctor } from '../../../models/doctor.model';
+import { DoctorService } from '../../../services/doctor.service';
 
 @Component({
   selector: 'app-doctors',
@@ -8,9 +11,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DoctorsComponent implements OnInit {
 
-  constructor() { }
+  public loading:boolean = true;
+  public doctors:Doctor[] = [];
+
+  constructor(private doctorService:DoctorService, private modalImageService: ModalImageService) { }
 
   ngOnInit(): void {
+    this.loadDoctors();
+  }
+
+  loadDoctors() {
+    this.loading = true;
+    this.doctorService.loadDoctors().subscribe(doctors => {
+      this.loading = false;
+      this.doctors = doctors;
+    });
+  }
+
+  openModal(doctor:Doctor) {
+    this.modalImageService.openModal('doctors', doctor.id, doctor.img);
   }
 
 }
--- src/app/services/doctor.service.ts
+++ src/app/services/doctor.service.ts
@@ -1,9 +1,48 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+import { map } from 'rxjs/operators';
+import { Doctor } from '../models/doctor.model';
+
+const base_url:string = environment.base_url;
 
 @Injectable({
   providedIn: 'root'
 })
 export class DoctorService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  get token():string {
+    return localStorage.getItem('token') || '';
+  }
+
+  get headers() {
+    return {
+      headers: {
+        'x-token': this.token
+      }
+    }
+  }
+
+
+  loadDoctors() {
+    const url = `${base_url}/doctors`;
+    return this.http.get(url, this.headers).pipe(map((response:{ok:boolean, doctors:Doctor[]}) => response.doctors));
+  }
+
+  createDoctor(doctor:Doctor) {
+    const url = `${base_url}/doctors`;
+    return this.http.post(url, doctor, this.headers);
+  }
+
+  updateDoctor(doctor:Doctor) {
+    const url = `${base_url}/doctors/${doctor.id}`;
+    return this.http.put(url, doctor, this.headers);
+  }
+
+  deleteDoctor(id_doctor:string) {
+    const url = `${base_url}/doctors/${id_doctor}`;
+    return this.http.delete(url, this.headers);
+  }
 }
